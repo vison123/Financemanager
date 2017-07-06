55ed7467722c49c1b7952a3c2e16a1a5dba42de1
Vison <723855399@qq.com>
2017-07-06 16:24:56 +0800

    Update DbScript.java

--- app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
+++ app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
@@ -9,3 +9,21 @@ CREATE TABLE Project(
    out_amount     DECIMAL(20,6),
    in_amount      DECIMAL(20,6)
 );
+
+CREATE TABLE Category(
+   categoty_id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   project_id       INTEGER,
+   name             VARCHAR(20),
+   category_budget  DECIMAL(20,6),
+   description      VARCHAR(255)
+);
+
+CREATE TABLE Money(
+   money_id    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   money_category_id     INTEGER,
+   project_id            INTEGER,
+   in_out_type           VARCHAR(20),
+   occur_date            DECIMAL(20,6),
+   amount                DECIMAL(20,6),
+   description           VARCHAR(255)
+);
