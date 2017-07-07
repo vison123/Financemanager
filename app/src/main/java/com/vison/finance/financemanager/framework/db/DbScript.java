2d46f7f169d01f2ccf5fafe1427208a96656ed03
Vison <723855399@qq.com>
2017-07-07 17:10:31 +0800

    Update DbScript.java

--- app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
+++ app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
@@ -27,3 +27,6 @@ CREATE TABLE Money(
    amount                DECIMAL(20,6),
    description           VARCHAR(255)
 );
+
+INSERT INTO Project (project_name,start_date,end_date,budget,in_amount,out_amount,comment,description)
+VALUES ('project1', '2017-07-01 11:21:10.64', '2018-07-01 11:21:10.64', 30000000, 20000, 200000, '','');
