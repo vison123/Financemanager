f236ae016f117630efa7eadd5821ce6b248b75d6
Vison <723855399@qq.com>
2017-07-07 17:40:19 +0800

    Update DbScript.java

--- app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
+++ app/src/main/java/com/vison/finance/financemanager/framework/db/DbScript.java
@@ -1,3 +1,4 @@
+// 建表语句，没有逻辑删除，直接删除数据
 CREATE TABLE Project(
    project_id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
    project_name   VARCHAR(50)    NOT NULL,
@@ -28,6 +29,7 @@ CREATE TABLE Money(
    description           VARCHAR(255)
 );
 
+// 初始数据
 INSERT INTO Project (project_name,start_date,end_date,budget,in_amount,out_amount,comment,description)
 VALUES ('project1', '2017-07-01 11:21:10.64', '2018-07-01 11:21:10.64', 30000000, 20000, 200000, '','');
 
@@ -45,5 +47,13 @@ VALUES (1,1,'IN','2017-07-08 11:21:10.64',30000,'');
 INSERT INTO Money (money_category_id,project_id,in_out_type,occur_date,amount,description)
 VALUES (1,1,'OUT','2017-07-09 11:21:10.64',40000,'');
 
-
+// 查询语句
+// 首页查询所有项目信息
+SELECT * from Project;
+//查询某项目下所有类别
+SELECT * from Category WHERE project_id = 1;
+//查询某类别下所有账单
+SELECT * from Money WHERE money_category_id = 1;
+//查询项目下所有账单
+SELECT * from Money WHERE project_id = 1;
 
